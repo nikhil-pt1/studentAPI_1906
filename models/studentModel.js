563fb04bdc2ad2a8b0b8c97059d432621d078c30
nikhil-pt1 <nikhil.parshtechnologies@gmail.com>
2025-06-19 13:21:57 +0530

    Add files via upload

--- models/studentModel.js
+++ models/studentModel.js
@@ -3,10 +3,26 @@ const mongoose=require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/students").then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));;
 
-const studentSchema=mongoose.Schema({
-  name: String,
-  address: String,
-  Phone: Number, 
-})
-
+const studentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true
+  },
+  address: {
+    type: String,
+    required: [true, 'Address is required'],
+    trim: true
+  },
+   Phone: {
+    type: Number,
+    required: [true, "Phone number is required"],
+  validate: {
+      validator: function(v) {
+        return /^[6-9]\d{9}$/.test(v); // basic Indian phone number pattern
+      },
+      message: props => `${props.value} is not a valid phone number`
+    }
+  }
+});
 module.exports=mongoose.model("student",studentSchema)
